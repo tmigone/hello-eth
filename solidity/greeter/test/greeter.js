8324821048e20eb2e8cec19cf61d8a43b4772710
Tomás Migone <tomasmigone@gmail.com>
2022-02-17 15:19:16 -0300

    greeter: update tests
    Signed-off-by: Tomás Migone <tomasmigone@gmail.com>

--- solidity/greeter/test/greeter.js
+++ solidity/greeter/test/greeter.js
@@ -6,29 +6,33 @@ const Greeter = artifacts.require("Greeter");
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("Greeter", function (/* accounts */) {
-  it("should assert true", async function () {
-    await Greeter.deployed();
-    return assert.isTrue(true);
+  it("should deploy contract successfully", async function () {
+    const instance = await Greeter.deployed();
+    assert(instance, "Not deployed");
   });
 
-  it("should greet us with default greeting", async function () {
-    const instance = await Greeter.deployed();
-    const greet = await instance.greet();
-    assert.equal(greet, "Hello, world!");
-  })
+  describe("greet()", function () {
+    it("should greet us with default greeting", async function () {
+      const instance = await Greeter.deployed();
+      const expected = "Hello, world!";
+      const actual = await instance.greet();
 
-  it("should update greeting using setGreeting", async function () {
-    const NEW_GREETING = "Goodbye, world!";
-    const instance = await Greeter.deployed();
-    await instance.setGreeting(NEW_GREETING);
-    assert.equal(instance.greeting(), NEW_GREETING);
-  })
+      assert.equal(actual, expected, `greeted with '${expected}'`);
+    });
+  });
+});
 
-  it("should greet us with a custom set greeting", async function () {
-    const NEW_GREETING = "G'day y'all!";
-    const instance = await Greeter.deployed();
-    await instance.setGreeting(NEW_GREETING);
-    const greet = await instance.greet();
-    assert.equal(greet, NEW_GREETING);
-  })
+// Make sure state changes stay isolated from other tests
+contract("Greeter: update greeting", function () {
+  describe("setGreeting()", function () {
+    it("should greet us with a custom set greeting", async function () {
+      const instance = await Greeter.deployed();
+      const expected = "G'day y'all!";
+
+      await instance.setGreeting(expected);
+      const actual = await instance.greet();
+
+      assert.equal(actual, expected, `greeted with '${expected}'`);
+    });
+  });
 });
