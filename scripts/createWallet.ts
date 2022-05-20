b3098ac85665ff177bad4a42438146ff272b5790
Tomás Migone <tomasmigone@gmail.com>
2022-05-20 14:59:16 -0300

    createWallet script: print mnemonic
    Signed-off-by: Tomás Migone <tomasmigone@gmail.com>

--- scripts/createWallet.ts
+++ scripts/createWallet.ts
@@ -3,3 +3,4 @@ import { ethers } from "ethers";
 const wallet = ethers.Wallet.createRandom()
 console.log(`Address: ${wallet.address}`);
 console.log(`Private key: ${wallet.privateKey}`);
+console.log(`Mnemonic: ${wallet.mnemonic}`);
